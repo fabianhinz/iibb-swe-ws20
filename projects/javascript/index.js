ef3e67425a07ca904817cc5108b14520ecaaade1
Fabian Hinz <hinzfabian.fh@gmail.com>
2020-11-16 17:32:53 +0100

    js lecture update

--- projects/javascript/index.js
+++ projects/javascript/index.js
@@ -29,14 +29,18 @@ obj.printToConsole();
 console.log(obj.name, obj['age']);
 
 // Arrays
-const arr = [1,2,3,4,5];
+const arr = [1, 2, 3, 4, 5];
 const lengthOfArray = arr.length;
 lengthOfArray;
 const secondEl = arr[1];
 secondEl;
 
 // filter 
-const elementsBiggerThanTwo = arr.filter(el => el > 2);
+// Array<number>.filter(predicate: (value: number, index: number, array: number[])): number[]
+const elementsBiggerThanTwo = arr.slice(1).filter(value => { 
+    debugger;
+    return value > 2 
+});
 elementsBiggerThanTwo;
 
 // push
@@ -48,8 +52,35 @@ const sumOfArr = arr.reduce((acc, current) => {
 sumOfArr;
 
 // Funktionen
-const fn = () => {};
+const fn = () => { };
 
 // Closures
+const useClosure = initialValue => {
+    let value = initialValue;
+    const increment = () => {
+        ++value;
+        console.log(value);
+    };
 
+    return increment;
+};
 
+const closureExample = useClosure(0);
+closureExample();
+closureExample();
+closureExample();
+
+// ohne destructering
+const obj_name = obj.name
+const obj_age = obj.age
+
+// Destructering
+const { age: localVariableAge } = obj
+
+// rest properties
+const { age, ...rest } = obj
+
+// spread properties
+const mergedObj = { age, ...rest }
+mergedObj
+obj
